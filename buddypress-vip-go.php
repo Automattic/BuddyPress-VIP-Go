b9dc77053a76682cae001f77e79f4f290744d1d3
Gary Jones <gary.jones@automattic.com>
2025-07-07 12:55:13 +0100

    fix: Add DOING_AJAX check for A8C_Files initialization
    This allows file handling amendments to work during Ajax requests too.

--- buddypress-vip-go.php
+++ buddypress-vip-go.php
@@ -25,7 +25,7 @@ defined( 'ABSPATH' ) || exit;
 add_action(
 	'bp_loaded',
 	function () {
-		if ( ! class_exists( 'A8C_Files' ) || ! defined( 'FILES_CLIENT_SITE_ID' ) || ! defined( 'FILES_ACCESS_TOKEN' ) ) {
+		if ( ! defined( 'DOING_AJAX' ) || ! class_exists( 'A8C_Files' ) || ! defined( 'FILES_CLIENT_SITE_ID' ) || ! defined( 'FILES_ACCESS_TOKEN' ) ) {
 			return;
 		}
 
